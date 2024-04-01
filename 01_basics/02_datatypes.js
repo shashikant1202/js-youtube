c3a8adb7b1de3a6a6857a0ea1e9ccc36cb54032d
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-04-01 13:13:20 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; //trear all JS code as newer version
+
+// alert("3+3") //we are using node js not browser
+
+/*console.log(3 
+    +
+     3)  //code readability should be high
+
+console.log("hitesh")*/
+
+let name = "hitesh"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+//number ==> 2 to power 53
+//bigint if no. is grater than above specified 
+//string ==> " " or ''
+//boolean ==> true/false
+//null ==> standalone value  it is a specific value not 0  
+//whwn we try to get the type of null we get object
+//undefined ==> the value is not defined yet
+//symbol ==> unique
+
+
+//object 
+console.log(typeof "hitesh"); //output==> string
+
+console.log(typeof age); //output==> number
+
+console.log(typeof undefined); //output==> undefined
+
+console.log(typeof null); //output==> object 
+
+/*
+  ECMA read the specification
+*/
