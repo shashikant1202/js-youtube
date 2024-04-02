fb67c13a75b2889e0d67e28b9cfe8d8176b5dc88
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-04-02 05:51:58 +0000

    conversion of datat types and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,8 +4,8 @@ console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" ==> 33
 // "33abc" ==> NaN
@@ -23,5 +23,50 @@ let isLoggedIn = "0"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);  
+// console.log(stringNumber);
+// console.log(typeof stringNumber);  
+
+
+// ******************* OPERATIONS *************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2); //conversion is done on the basis of first data types in the console 
+
+// //if first Data Types of operation is string then the whole operation will be treated as same data types   
+    
+// console.log(1 + 2 + "2");  
+// // & if string is in last then previous operation will be done 
+
+// console.log((3 + 4) * 5 % 3);
+
+
+console.log(+true);
+console.log(+"");
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCenter = 100
+++gameCenter;
+console.log(gameCenter);
+
+
