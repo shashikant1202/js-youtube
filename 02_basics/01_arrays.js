62ec489ca5731ac5ed99528092f497079e7f052b
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-05-07 01:37:52 +0000

    its not yet completed

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+//arrays
+// () ------> PARENTHESIS, {}  -----> CURLY BRACES, [] ----> BRACKETS
+
+// *************** Array Declration **********
+const myArr = [0, 1, 2, 3, 4, 5] // js array ---> storing multiple items under a single variable.
+// js array ---> resizable, can be different data types in a one variable.
+//js array copy operation create SHAllOW copies(in which the change i made in the copied array will also be happen in original array) & DEEP copies(copied array and original are independent to each other) 
+
+const myHeros = ["shaktiman", "naagraj"] //array can be declare like this
+
+const myArr2 = new Array(1, 2, 3, 4) // //array can also be declare like this
+
+console.log(myArr[1]); // Indexes(position) in Js starts from 0 then 1 then 2 like this and last index is total length - 1
+
+
+// **************Array Methods******************
