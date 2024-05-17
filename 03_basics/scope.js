98f1cf7fe8b64cb5b57b81b01e89574b7f26776e
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-05-17 04:35:14 +0000

    scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,21 +1,24 @@
-// let a = 10 
+let a = 300
 // const b = 20
 // var c = 30
 
-// {} is called scope in function , if else etc but in object it  is object declaration
+// {} is called scope in function , if , else etc but in object it  is object declaration
 
 if (true) {           // if ke andar jo code likha hai use kahte hai block scope and iske bahar ke code ko kahte hai global scope
     let a = 10 
     const b = 20
-    var c = 30
-}
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-     
+    // var c = 30/
+    console.log("INNER: ", a);   //block scope  // inside the curly braces
 }
 
-// browser ke cosole codimg envitomen6 g0 [[me  global space diffrenrt hota hai but  
 
-// console.log(a);
+console.log(a); // global scope  // outside the {}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];   
+// }
+
+// browser ke console aur code editor ka global space diffrenrt hota hai but  
+
 // console.log(b);
-console.log(c);  //yaha par var jo if ke andaar hai wo baharbhi c ka value de raha hai jo ki ek denerit hai isi karan we dont use var
+// console.log(c);  //yaha par var jo if ke andaar hai wo bahar bhi c ka value de raha hai jo ki ek demerit hai isi karan we don't use var
