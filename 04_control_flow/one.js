a2f650691c068c623b03aecc635d9306d60816ea
shashikant1202 <162179698+shashikant1202@users.noreply.github.com>
2024-05-21 12:19:09 +0530

    adhura

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+//if
+
+// if (true) {   //if true then go into it
+    
+// }
+
+const isUserLoggedIn = true
+
+if (2 == "2") {     
+    console.log("executed");
+}
+
+
+// operator -----> 
+// < // less than
+// >  // greater than 
+// <= // less than or equal to 
+// >= // greater than or equal to
+// == // Equal to,   It only checks the value inside or outside of the ".."
+// === // strict equal, It checks the data type also
+// !=  // not equal to 
