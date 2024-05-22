1ba9ccca5c7df139e4ea6dc0b94b4fc5f4b441a0
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-05-22 07:16:30 +0000

    truthy & falsy done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -17,7 +17,7 @@ if (userEmail) {
 
 
 // detecting array is empty or not
-if (userEmail.length === 0) {
+if (userEmail.length === 0) {  
     console.log("Array is empty");
 }
 
@@ -30,14 +30,26 @@ if (Object.keys(emptyObj).length === 0) {
 }
 
 
-//Nullish Coalescing Operator (???): null undefined
+//Nullish Coalescing Operator (???): null  & undefined
 
+ //Is operatoe me sabse pahle sefty  check j=hota hai  jisme agar null ya undefined aa jaye to usko skip kar ke next value ya jo next valid value rahta hai usi ko execute kar deta hai
+
+ // agar kabhi error aaye to hum use kis tarah handle kare usi pr ye  Nullish coalescing operator based hai
 
 let val1;
-val1 = 5 ?? 10
+// val1 = 5 ?? 10  // -----> 5
+val1 = null ?? 10  // -----> 10 
+// val1 = undefined ?? 15  // ------> 15
+// val1 = null ? 10 ?? 15 // ------> 10
 
- 
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const IceTeaPrice = 100
+IceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
-lpgtauer;
 
