d80e42e25fd14f461954608debee0dc4e4c3f23c
shashikant1202 <162179698+shashikant1202@users.noreply.github.com>
2024-05-24 19:01:42 +0530

    still need some changs in it

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,24 +1,25 @@
 // Js me jo default hote hai wo hamesha Captail letter se suru hote hai 
 const score = 400
-console.log(score); //js can automatically detect the data types but we can also define it
+console.log(score); // -------> 400 //js can automatically detect the data types but we can also define it
 
 const balance = new Number(100) //exclusively define the object type which is the number
-console.log(balance);
+console.log(balance);  // ---------> [Number: 100]
 
-console.log(balance.toString());   // number to string
-console.log(balance.toString().length); //here we got the length of the convertd string from number
+console.log(balance.toString()); // ---> 100  // number to string
 
-console.log(balance.toFixed(2));  // it gives the precision value  means VALUE IN DECIMAL POINTS 
+console.log(balance.toString().length); // ----> 3 //here we got the length of the convertd string from number
+
+console.log(balance.toFixed(2)); // -----> 100.00 // it gives the precision value  means VALUE IN DECIMAL POINTS // here 2 gives the precision value till 2 decimal point
 
 const otherNumber = 123.8987
 
-console.log(otherNumber.toPrecision(4)); // it also gives the precise value upto the given digits in the  .toPrecision from left(starting) side of the number
+console.log(otherNumber.toPrecision(4)); // ----> 123.9 // it also gives the precise value upto the given digits in the  .toPrecision from left(starting) side of the number
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN')); // by default it is in the US standard but we can change it to our standard 'en-IN' 
+console.log(hundreds.toLocaleString('en-IN')); // ------> 10,00,000// by default it is in the US standard but we can change it to our standard 'en-IN' 
 //us standars 100,000,000  INDIAN standard 1,00,00,000 we make like this by toLocaleString
 
-Number  // put it in the cosole of browaer insepct
+Number  // put it in the cosole of browser insepct
 Number.MAX_VALUE   // it will give the maximum value of a number can be possible is 
 Number.MAX_SAFE_INTEGER // it will give the maximum integer which can be stored
 
