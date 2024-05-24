562929f6b0c641bb46568a5b9f7317473366f8b0
shashikant1202 <162179698+shashikant1202@users.noreply.github.com>
2024-05-24 15:50:55 +0530

    some changes in the comments

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof score);  // ------> boolean
+console.log(typeof(score));  //----------> boolean
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber); //-------> number
+console.log(valueInNumber); // -----> 1
 
 // "33" ==> 33
 // "33abc" ==> NaN
@@ -14,8 +14,8 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "0"
 
-// let booleanisLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanisLoggedIn);
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);  // ---------> true
 
 // 1 ==> true; 0==> false
 // "hitesh" ==> true
@@ -23,15 +23,15 @@ let isLoggedIn = "0"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);  
+console.log(stringNumber);  // ----------> 33
+console.log(typeof stringNumber);  // --------> string
 
 
 // ******************* OPERATIONS *************************
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+console.log(negValue); // ----------> -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,29 +44,29 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-console.log(str3);
+console.log(str3);  //----------> hello hitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + "2");
-// console.log("1" + 2 + 2); //conversion is done on the basis of first data types in the console 
+console.log("1" + 2);  // --------> 12
+console.log(1 + "2"); //--------> 12
+console.log("1" + "2"); //--------> 12
+console.log("1" + 2 + 2); // -------> 122 //conversion is done on the basis of first data types in the console 
 
 // //if first Data Types of operation is string then the whole operation will be treated as same data types   
     
-// console.log(1 + 2 + "2");  
-// // & if string is in last then previous operation will be done 
+console.log(1 + 2 + "2");    // ----------> 32
+// //  if string is in last then previous operation will be done on the basis of their data types
 
-// console.log((3 + 4) * 5 % 3);
+console.log((3 + 4) * 5 % 3); // ---------->  2
 
 
-console.log(+true);
-console.log(+"");
+console.log(+true); //-------> 1
+console.log(+""); //--------> 0
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCenter = 100
 ++gameCenter;
-console.log(gameCenter);
+console.log(gameCenter);  //----------> 101
 
 
