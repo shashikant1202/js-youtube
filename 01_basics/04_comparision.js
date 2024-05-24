cf5a3211a06cc252125da47cb0334ac5bdaaaaa6
shashikant1202 <162179698+shashikant1202@users.noreply.github.com>
2024-05-24 16:13:27 +0530

    some changes in comment

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,25 +1,25 @@
-// console.log(2>1)
-// console.log(2>= 1)
-// console.log(2 < 1)
-// console.log(2 == 1)
-// console.log(2 != 1)
+console.log(2>1) // ------> true
+console.log(2>= 1) // --------> true
+console.log(2 < 1) // --------> false
+console.log(2 == 1) // ----------> false
+console.log(2 != 1)  // ---------> true
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);  // ----------> true
+console.log("02" > 1);  // ---------> true
 
-
+console.log("Now Next Part");
        // ************CONFUSING CONVERSION**************
      // *****Generally We Avoid These Type Conversion*****
 
-console.log(null > 0);
-console.log(null == 0); //comparison 
-console.log(null >= 0); //here null is getting converted into 0
+console.log(null > 0);  // -----------> false
+console.log(null == 0); //comparison   // ------> false
+console.log(null >= 0); //here null is getting converted into 0  //----------> true
 
 console.log(undefined == 0); 
 console.log(undefined > 0); 
 console.log(undefined < 0); 
 
-//  ===   --> Strict check  By using (===) we strictly check the data types of this operation also because by using  (==) this conversion is being done 
+//  ===   --> Strict equal to check  By using (===) we strictly check the data types and value of this operation also because by using  (==) this only value is being check  
 
-console.log("2" === 2);
+console.log("2" === 2);  // --------> false
